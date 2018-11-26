f4ed9aa06009d1d33e1c067f6510dfa9dbec3818
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2018-11-26 00:27:22 -0500

    Add fillable array to Portfolio

--- StockPortfolio/app/Portfolio.php
+++ StockPortfolio/app/Portfolio.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['cash_owned'];
 }

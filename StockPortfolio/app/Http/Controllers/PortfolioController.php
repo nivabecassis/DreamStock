80d94d402385affbcc667537fa21388ae33e7a15
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2018-11-28 23:20:44 -0500

    Add getPortfolioValue function in PortfolioController

--- StockPortfolio/app/Http/Controllers/PortfolioController.php
+++ StockPortfolio/app/Http/Controllers/PortfolioController.php
@@ -20,6 +20,39 @@ class PortfolioController extends Controller
         $this->middleware('auth');
     }
 
+    public function index()
+    {
+        // Get current authenticated user
+        $user = Auth::user();
+
+        // Get portfolio value
+        $portfolioValue = $this->getPortfolioValue($user);
+
+        return view('home', [
+            'portfolioValue' => $portfolioValue,
+        ]);
+    }
+
+    /**
+     * Gets total sum of all current portfolio value
+     * 
+     * @return totalValue Sum of all current portfolio value
+     */
+    public function getPortfolioValue($user)
+    {
+        // Get authenticated user's metadatas
+        $user->portfolios->portfolio_stocks;
+        $stocks = $user->portfolios->portfolio_stocks;
+
+        // Sum all purchase_price to determine the portfolio value
+        $totalValue = 0;
+        foreach ($stocks as $value) {
+            $totalValue += $value->purchase_price;
+        }
+
+        return $totalValue;
+    }
+
     /**
      * Display user balance
      *
@@ -31,5 +64,5 @@ class PortfolioController extends Controller
         // Not sure if this works properly yet
         $portfolio = User::find($username)->portfolios();
         return $portfolio->cash_owned;
-    } // getBalance()
+    }
 }

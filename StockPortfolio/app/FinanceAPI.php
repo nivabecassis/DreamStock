1009e2c80cd20de25ae1dc9d0de76eca9d5816a0
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2018-11-30 16:46:26 -0500

    Update FinanceAPI

--- StockPortfolio/app/FinanceAPI.php
+++ StockPortfolio/app/FinanceAPI.php
@@ -7,21 +7,6 @@ use App\ApiUri;
 class FinanceAPI 
 {
     /**
-     * Gets stock info as associative array
-     *
-     * @param $ticker Company to get stock information for
-     * @return json object
-     */
-    public static function getStockInfo($ticker)
-    {
-        $query = "?symbol=" . $ticker . "&api_token=" . APIKEY;
-        $strJson = file_get_contents(WORLDTRADINGDATA . $query);
-
-        return json_decode($strJson);
-    }
-
-
-    /**
      * Gets stock info for all tickers
      *
      * @param $tickers Companies to get quotes from
@@ -37,7 +22,7 @@ class FinanceAPI
         $symbols = substr($symbols, 0 , strlen($symbols ) - 1);
         $strJson = file_get_contents(WORLDTRADINGDATA . $symbols . $apiKey);
 
-        return json_decode($strJson);
+        return json_decode($strJson, true);
     }
 }
 

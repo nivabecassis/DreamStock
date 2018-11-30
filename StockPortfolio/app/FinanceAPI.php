5c16f152f60c3ed03eafe09a84426f1608e9c6ba
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2018-11-30 16:19:05 -0500

    Remove conflict message. Update method return type to associative array

--- StockPortfolio/app/FinanceAPI.php
+++ StockPortfolio/app/FinanceAPI.php
@@ -9,30 +9,12 @@ namespace App;
 class FinanceAPI
 {
     /**
-<<<<<<< HEAD
-     * Gets stock info as associative array
-     *
-     * @param $ticker Company to get stock information for
-     * @return json object
-     */
-    public static function getStockInfo($ticker)
-    {
-        $query = "?symbol=" . $ticker . "&api_token=" . APIKEY;
-        $strJson = file_get_contents(WORLDTRADINGDATA . $query);
-
-        return json_decode($strJson);
-    }
-
-
-    /**
-=======
->>>>>>> 790cb85de0f83746754b4a7e2cb6f80aefdb396b
      * Gets stock info for all tickers
      *
      * @param $tickers Companies to get stock information for
-     * @return json object
+     * @return associativeArray object
      */
-    static function getAllStockInfo(array $tickers)
+    public static function getAllStockInfo(array $tickers)
     {
         $symbols = "?symbol=";
         $apiKey = "&api_token=" . APIKEY;
@@ -42,6 +24,6 @@ class FinanceAPI
         $symbols = substr($symbols, 0 , strlen($symbols ) - 1);
         $strJson = file_get_contents(WORLDTRADINGDATA . $symbols . $apiKey);
 
-        return json_decode($strJson);
+        return json_decode($strJson, true);
     }
 }

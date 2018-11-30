335a5ffde359cfcbe587db13627d5c8fb0f0b306
=Austin Antoine <=austinantoine@hotmail.com>
2018-11-29 20:08:34 -0500

    Create FinanceAPI file and add to ApiUri file new constants

--- StockPortfolio/app/ApiUri.php
+++ StockPortfolio/app/ApiUri.php
@@ -6,3 +6,11 @@ namespace App;
 if (!defined('CURRENCYEXCHANGE')) { // Check if constant exists already
     define("CURRENCYEXCHANGE", "https://www.freeforexapi.com/api/live?pairs=");
 }
+
+if (!defined('WORLDTRADINGDATA')) {
+    define("WORLDTRADINGDATA", "https://www.worldtradingdata.com/api/v1/stock");
+}
+
+if (!defined('APIKEY')) {
+    define("APIKEY", "mciLyGJgY1OsX0JXQOhRsG45bziDeiqG00eKT7ucc5bP3Gj21eWuT9GTC7Jw");
+}

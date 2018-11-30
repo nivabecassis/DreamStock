e2cca757ddccb4ecc835928a147edc509fea04f0
Stephen Kwan <stephen.kwan@dawsoncollege.qc.ca>
2018-11-29 22:29:59 -0500

    Turn FinanceAPI into a class

--- StockPortfolio/app/FinanceAPI.php
+++ StockPortfolio/app/FinanceAPI.php
@@ -4,17 +4,19 @@ namespace App;
 
 use App\ApiUri;
 
-/**
- * Gets stock info as associative array
- *
- * @param $ticker Company to get stock information for
- * @return associative array
- */
-if (!function_exists('getStockInfo')) { // Checks if function is already defined
-    function getStockInfo($ticker)
+class FinanceAPI 
+{
+    /**
+     * Gets stock info as associative array
+     *
+     * @param $ticker Company to get stock information for
+     * @return associative array
+     */
+    public static function getStockInfo($ticker)
     {
         $url = WORLDTRADINGDATA;
-        $query = "?symbol=" . $ticker . "& api_token=" . APIKEY;
-        return json_decode(file_get_contents($url . $query));
+        $query = "?symbol=" . $ticker . "&api_token=" . APIKEY;
+
+        return file_get_contents($url . $query);
     }
 }

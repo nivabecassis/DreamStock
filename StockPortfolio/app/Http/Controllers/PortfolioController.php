3acbb4ef89ba1b4b125ef6841fa9217d27609006
Stephen Kwan <stephen.kwan@dawsoncollege.qc.ca>
2018-11-29 22:31:06 -0500

    Add implementation to get the current portfolio value

--- StockPortfolio/app/Http/Controllers/PortfolioController.php
+++ StockPortfolio/app/Http/Controllers/PortfolioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\User;
+use App\FinanceAPI;
 
 /*
  * Portfolio controller
@@ -20,10 +21,10 @@ class PortfolioController extends Controller
 
     public function index()
     {
-        // Get current authenticated user
+        // Gets current authenticated user
         $user = Auth::user();
 
-        // Get portfolio value
+        // Gets portfolio value
         $portfolioValue = $this->getPortfolioValue($user);
 
         return view('home', [
@@ -38,17 +39,20 @@ class PortfolioController extends Controller
      */
     public function getPortfolioValue($user)
     {
-        // Get authenticated user's metadatas
+        // Gets authenticated user's metadatas
         $user->portfolios->portfolio_stocks;
-        $stocks = $user->portfolios->portfolio_stocks;
 
-        // Sum all purchase_price to determine the portfolio value
-        $totalValue = 0;
-        foreach ($stocks as $value) {
-            $totalValue += $value->purchase_price;
-        }
+        // Loads all metadata
+        $stocksData = $user->portfolios->portfolio_stocks;
 
-        return $totalValue;
+        // Gets all user's tickers
+        $tickers = $this->getAllTickers($stocksData);
+
+        // Gets all user's prices
+        $prices = $this->getAllPrices($tickers);
+
+        // Returns sum of all prices
+        return $this->sumAll($prices);
     }
 
     /**
@@ -61,5 +65,57 @@ class PortfolioController extends Controller
     {
         // Not sure if this works properly yet
         return $user->portfolios->cash_owned;
+    }
+
+    /**
+     * Gets all ticker symbols from authenticated user
+     * 
+     * @param metadata User's metedata
+     * @return array Array of company tickers
+     */
+    private function getAllTickers($metadata) 
+    {
+        $companiesTicker = array();
+        foreach ($metadata as $value) {
+            array_push($companiesTicker, $value->ticker_symbol);
+        }
+
+        return $companiesTicker;
+    }
+
+    /**
+     * Gets all prices from authenticated user
+     * 
+     * @param tickers User's tickers
+     * @return array Array of current prices
+     */
+    private function getAllPrices(array $tickers)
+    {
+        // TODO: Rename 'getStockInfo' function name when supported function is coded in FinanceAPI 
+        $strJson = FinanceAPI::getStockInfo($tickers);
+        $json = json_decode($strJson)->data;
+
+        $currentPrices = array();
+        foreach ($json as $value) {
+            array_push($currentPrices, $value->price);
+        }
+
+        return $currentPrices;
+    }
+
+    /**
+     * Sums all prices
+     * 
+     * @param array Array containing every prices
+     * @return sum Sum of all prices
+     */
+    private function sumAll(array $prices) 
+    {
+        $sum = 0;
+        foreach ($prices as $value) {
+            $sum += $value;
+        }
+
+        return $sum;
     }
 }

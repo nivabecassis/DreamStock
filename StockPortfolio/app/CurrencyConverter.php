3aa855d5ea1c787d4dbccba9a173fa0ba53b03d1
Stephen Kwan <stephen.kwan@dawsoncollege.qc.ca>
2018-11-29 22:33:40 -0500

    Change CurrencyConverter into a class

--- StockPortfolio/app/CurrencyConverter.php
+++ StockPortfolio/app/CurrencyConverter.php
@@ -4,18 +4,19 @@ namespace App;
 
 use App\ApiUri;
 
-/**
- * Converts any type of currency that 'FreeForexAPI' supports to USD
- * 
- * @param $currency Original currency (e.g 'CAD', 'JPY, 'EUR', ...)
- * @param $value Original value (e.g '9', '9.0', '99.00', ...)
- * @return $value under USD currency
- */
-if (!function_exists('convertToUSD')) { // Checks if function is already defined
-    function convertToUSD($currency, $value)
+class CurrencyConverter
+{
+    /**
+     * Converts any type of currency that 'FreeForexAPI' supports to USD
+     * 
+     * @param $currency Original currency (e.g 'CAD', 'JPY, 'EUR', ...)
+     * @param $value Original value (e.g '9', '9.0', '99.00', ...)
+     * @return $value under USD currency
+     */
+    public static function convertToUSD($currency, $value)
     {
         $pairs = "USD" . $currency;
-        $json =  file_get_contents(CURRENCYEXCHANGE . $pairs);
+        $json = file_get_contents(CURRENCYEXCHANGE . $pairs);
         $data = json_decode($json);
         $rate = $data->rates->$pairs->rate;
 

3d720cabea42ee39abd549411f2366e0d806e8e3
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2018-12-03 12:36:55 -0500

    Add implementation to sell shares

--- StockPortfolio/app/Http/Controllers/ApiController.php
+++ StockPortfolio/app/Http/Controllers/ApiController.php
@@ -67,4 +67,88 @@ class ApiController extends Controller
             return response()->json($stocks, 200);
         }
     }
+
+    /**
+     * Performs sell share action against the database and returns 
+     * the user's remaining cash.
+     * 
+     * Method type: POST
+     * Success response: 200
+     * Error response: 400 => invalid_token
+     *                 401 => invalid_ticker_or_quantity
+     * 
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sellStock(Request $request)
+    {
+        // Loads user; returns null if invalid
+        $user = auth('api')->user();
+
+        // Loads inputs
+        $ticker = $request->input("ticker");
+        $quantity = $request->input("quantity");
+
+        // Checks if user is invalid
+        if (!isset($user)) {
+            return response()->json(['error' => 'invalid_token'], 401);
+        }
+
+        // Checks if ticker and quantity are valid
+        if (!$this->isValidSellTicker($user, $ticker) || !$this->isValidSellQuantity($user, $quantity, $ticker)) {
+            return response()->json(['error' => 'invalid_ticker_or_quantity'], 400);
+        }
+        
+        // Sells shares
+        UserUtility::sellShares($user, $request->input("ticker"), $request->input("quantity"));
+        return response()->json(['cashleft' => $user->portfolios->cash_owned], 200);
+    }
+
+    /**
+     * Checks if quantity is valid
+     * 
+     * @param integer Quantity number
+     * @param string Ticker name
+     * @return bool True if it's valid else false
+     */
+    private function isValidSellQuantity($user, $quantity, $ticker) 
+    {
+        // Load all metadata
+        $stocks = $user->portfolios->portfolio_stocks;
+
+        // Check if quantity is a number
+        if (!is_numeric($quantity)) {
+            return false;
+        }
+
+        // Check if quantity is lesser or equal than the company's share count
+        foreach ($stocks as $stock) {
+            if ($ticker === $stock->ticker_symbol && $quantity <= $stock->share_count) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if ticker is valid
+     * 
+     * @param string Ticker name
+     * @return bool True if it's valid else false
+     */
+    private function isValidSellTicker($user, $ticker)
+    {
+        // Load all metadata
+        $stocks = $user->portfolios->portfolio_stocks;
+
+        // Check if ticker symbol exists in the database
+        foreach ($stocks as $stock) {
+            if ($ticker === $stock->ticker_symbol) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }

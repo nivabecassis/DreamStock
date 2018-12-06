a755f30461f76fdfcbd960f8aef02b97083f01f8
Stephen Kwan <stephen.kwan@dawsoncollege.qc.ca>
2018-12-06 17:19:53 -0500

    Update hamburger icon

--- StockPortfolio/resources/views/layouts/app.blade.php
+++ StockPortfolio/resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon" style="background-image: url({{ asset('svg/custom-hamburger.svg') }});"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
